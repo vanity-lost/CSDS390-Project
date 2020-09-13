a62ce2fd99c072ff428a4cfb84efd021291c4a4f
Sam <sammystinky@gmail.com>
2020-09-13 09:13:49 -0400

    Movement and Look
    Basic player movement (WASD) and player look by mouse (left right, capped up down). Made main scene with player and ground.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movespeed = 4.0f;
+    public float sprintspeed = 4.0f * 1.5f;
+    private CharacterController character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMovement = Input.GetAxis("Horizontal") * movespeed;
+        float yMovement = Input.GetAxis("Vertical") * movespeed;
+        Vector3 moving = new Vector3(xMovement, 0, yMovement);
+        
+        moving = Vector3.ClampMagnitude(moving, movespeed);
+        moving = moving * Time.deltaTime;
+        moving = transform.TransformDirection(moving);
+
+        character.Move(moving);
+    }
+}
---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    float ySensitivity = 8.0f;
+    float xSensitivity = 8.0f;
+    public float highY = 45.0f;
+    public float lowY = -45.0f;
+
+    float rotationY = 0f;
+
+    public enum Rotation {
+        x = 1,
+        y = 2,
+    }
+
+public Rotation rotation = Rotation.x;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotation == Rotation.x)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * xSensitivity, 0);
+
+        }
+        else
+        {   
+            rotationY -= Input.GetAxis("Mouse Y") * ySensitivity;
+            rotationY = Mathf.Clamp(rotationY, lowY, highY);
+            transform.localEulerAngles = new Vector3(rotationY, transform.localEulerAngles.y, 0);
+        }
+        
+    }
+}
