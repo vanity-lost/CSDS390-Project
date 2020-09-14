064e6f01b0f28d3bc3b0fe194a847dcae938f187
Randolph Zhao <randolph.zhao.49@gmail.com>
2020-09-13 21:40:11 -0400

    Basic UI element, with exit, controls remapping

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+
+}
