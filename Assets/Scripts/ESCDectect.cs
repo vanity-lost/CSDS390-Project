a8acd162ea503949ce77ca9971c1d7f2f01dd17e
Randolph Zhao <randolph.zhao.49@gmail.com>
2020-09-19 23:31:04 -0400

    Update UI change with resolution, adding new Canvas for each layers

--- Assets/Scripts/ESCDectect.cs
+++ Assets/Scripts/ESCDectect.cs
@@ -5,13 +5,17 @@ using UnityEngine.UI;
 
 public class ESCDectect : MonoBehaviour
 {
-    public GameObject ESCPanel;
+    public GameObject Panel1;
+    public GameObject Panel2;
+    public GameObject Panel3;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            ESCPanel.SetActive(!ESCPanel.activeSelf);
+            Panel1.SetActive(!Panel1.activeSelf);
+            Panel2.SetActive(false);
+            Panel3.SetActive(false);
         }
     }
 }
