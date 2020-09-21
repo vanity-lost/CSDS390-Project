78fbd9e17676a49fb32980cb50ef29eabae18f0d
Randolph Zhao <randolph.zhao.49@gmail.com>
2020-09-20 22:08:22 -0400

    Added some in-game UI

---
+++ Assets/Scripts/tabONOFF.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tabONOFF : MonoBehaviour
+{
+    public GameObject Tab;
+    
+    public void onClick(){
+        Tab.SetActive(!Tab.activeSelf);
+    }
+}
