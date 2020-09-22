cb4c76cfdc81389308fad75ebf6d7d9868989518
Alvin-Kong <axk1079@case.edu>
2020-09-22 02:27:21 -0700

    imported sub layout and added UI
    created a slider to keep track of sub's traveled distance with script to increase traveled distance after every second

---
+++ Assets/Scripts/SubDistanceTracker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class SubDistanceTracker : MonoBehaviour
+{
+    public Slider DistanceMeter;
+    public bool engineRun = true;
+    private float tDistance = 0f;
+    private float move = 0.1f;
+    private float maxDistance = 1.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        while(engineRun)
+        {
+            Thread.Sleep(1000);
+            tDistance += move;
+            DistanceMeter.value = tDistance;
+
+            if(tDistance == maxDistance)
+            {
+                engineRun = false;
+            }
+        }
+    }
+}
