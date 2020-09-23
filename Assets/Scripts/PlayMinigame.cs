eda1e4a9c91a9b9ba3667188750c8cbf81cf1c44
Sam <sammystinky@gmail.com>
2020-09-23 15:18:28 -0400

    Allows Player to Play Minigames by Pressing P or L
    P is fix engine
    L is connect wires

---
+++ Assets/Scripts/PlayMinigame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMinigame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            Debug.Log("fix Engine");
+            SceneManager.LoadScene("Fix Engine");
+        }
+        if (Input.GetKeyDown("l"))
+        {
+            SceneManager.LoadScene("Connect Wire");
+        }
+    }
+}
