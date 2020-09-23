b7ddc67929b249b736fe4f8e07f477fde376425a
NickMagic25 <37313679+NickMagic25@users.noreply.github.com>
2020-09-23 11:56:40 -0400

    Added UI elements in a sample scene
    Added UI elements that I worked on during the last two weeks to a sample scene.

---
+++ Assets/UI Scripts/PlayerIcon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// taken from a tutorial https://www.youtube.com/watch?v=S3pjBQObC90
+// modified to fit the context of the project
+public class PlayerIcon : MonoBehaviour
+{
+    public float speed=5f;
+    public void Update(){
+        float rotx = Input.GetAxis("Mouse X")*speed*Mathf.Deg2Rad;
+        float roty = Input.GetAxis("Mouse Y")*speed*Mathf.Deg2Rad;
+
+        transform.RotateAround(new Vector3(0f,0f,1f), -rotx);
+    }
+}
---
+++ Assets/UI Scripts/TaskPull.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskPull : MonoBehaviour
+{
+    [SerializeField] private GameObject task;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("e")){
+            task.SetActive(true);
+        }
+        else if(Input.GetKeyDown("g")){
+            task.SetActive(false);
+        }
+    }
+}
