20c9c767326a98525db3384c333f0b3e8f2522c4
Sam <sammystinky@gmail.com>
2020-09-27 14:14:29 -0400

    Repair Hull Minigame
    Cover leaks in sub with metal slabs

---
+++ Assets/Scripts/Repair Hull/LeakTracker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeakTracker : MonoBehaviour
+{
+    [SerializeField] private int leaks = 4;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LeakFilled()
+    {
+        leaks = leaks - 1;
+        if (leaks == 0)
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
