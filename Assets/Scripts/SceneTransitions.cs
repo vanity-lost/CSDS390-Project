97a65b5e507b6d278bc18e93df417d2145ed55f5
NickMagic25 <37313679+NickMagic25@users.noreply.github.com>
2020-09-28 15:40:58 -0400

    Added Death Scene, began work on scene transitions

---
+++ Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+// taken from https://www.youtube.com/watch?v=Qd2em_ts5vs
+public class SceneTransitions : MonoBehaviour
+{
+    public Animator transitionAnim;
+    public string sceneName;
+    public string sceneName2;
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            StartCoroutine(LoadScene(sceneName));
+        }
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            StartCoroutine(LoadScene(sceneName2)); 
+        }
+    }
+    IEnumerator LoadScene(string s){
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(s);
+    }
+}
