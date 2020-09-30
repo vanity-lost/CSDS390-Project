0b078fd19f890a0ab1965cf1068a23b24bb4ee30
Randolph Zhao <randolph.zhao.49@gmail.com>
2020-09-30 15:40:20 -0400

    Commit

--- Assets/Scripts/CreatureBehavior/indoorCreatureBehavior.cs
+++ Assets/Scripts/CreatureBehavior/indoorCreatureBehavior.cs
@@ -3,75 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class indoorCreatureBehavior : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+{   
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
         
-    }
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // // Update is called once per frame
+    // void Update()
+    // {
 
-        // Decides creature's actions
-        if (getCircuitWorkingStatus())
-        {
-            if (isWithinSensingRange(getCreatureLocation(), getPlayerLocation()) {
-                moveAway();
-            }
-            else
-            {
-                if (isWithinCircuitRange(getCreatureLocation()) {
-                    breakCircuits();
-                }
-                else
-                {
-                    moveToCircuits();
-                }
-            }
-        }
-        else
-        {
-            if (isWithinAttackRange(getCreatureLocation(), getPlayerLocation()) {
-                attack();
-            }
-            else
-            {
-                moveToPlayer();
-            }
-        }
+    //     // Decides creature's actions
+    //     if (getCircuitWorkingStatus())
+    //     {
+    //         if (isWithinSensingRange(getCreatureLocation(), getPlayerLocation())) {
+    //             moveAway();
+    //         }
+    //         else
+    //         {
+    //             if (isWithinCircuitRange(getCreatureLocation()) {
+    //                 breakCircuits();
+    //             }
+    //             else
+    //             {
+    //                 moveToCircuits();
+    //             }
+    //         }
+    //     }
+    //     else
+    //     {
+    //         if (isWithinAttackRange(getCreatureLocation(), getPlayerLocation()) {
+    //             attack();
+    //         }
+    //         else
+    //         {
+    //             moveToPlayer();
+    //         }
+    //     }
 
-        // Gets location of creature (from another class)
-        private Vector3 getCreatureLocation() { }
+    //     // Gets location of creature (from another class)
+    //     private Vector3 getCreatureLocation() { }
 
-        // Gets location of player (from another class) 
-        private Vector3 getPlayerLocation() { }
+    //     // Gets location of player (from another class) 
+    //     private Vector3 getPlayerLocation() { }
 
-        // True if player is within attacking range; false otherwise
-        private bool isWithinAttackRange(Vector3 creatureLocation, Vector3 playerLocation) { }
+    //     // True if player is within attacking range; false otherwise
+    //     private bool isWithinAttackRange(Vector3 creatureLocation, Vector3 playerLocation) { }
 
-        // True if player is within sensing range; false otherwise
-        private bool isWithinSensingRange(Vector3 creatureLocation, Vector3 playerLocation) { }
+    //     // True if player is within sensing range; false otherwise
+    //     private bool isWithinSensingRange(Vector3 creatureLocation, Vector3 playerLocation) { }
 
-        // True if player is within range to sabatage circuit; false otherwise
-        private bool isWithinCircuitRange(Vector3 creatureLocation) { }
+    //     // True if player is within range to sabatage circuit; false otherwise
+    //     private bool isWithinCircuitRange(Vector3 creatureLocation) { }
 
-        // Gets status of circuits (from another class)
-        private bool getCircuitWorkingStatus() { }
+    //     // Gets status of circuits (from another class)
+    //     private bool getCircuitWorkingStatus() { }
 
-        // Creature attacks player
-        private void attack() { }
+    //     // Creature attacks player
+    //     private void attack() { }
 
-        // Creature moves towards player
-        private void moveToPlayer() { }
+    //     // Creature moves towards player
+    //     private void moveToPlayer() { }
 
-        // Creature breaks circuits
-        private void breakCircuits() { }
+    //     // Creature breaks circuits
+    //     private void breakCircuits() { }
 
-        // Creature moves towards circuits
-        private void moveToCircuits() { }
+    //     // Creature moves towards circuits
+    //     private void moveToCircuits() { }
 
-        // Creature runs away from player
-        private void moveAway() { }
+    //     // Creature runs away from player
+    //     private void moveAway() { }
     }
