90d735b0752b448ea1bad4f28d285c3861362990
JKupoluyi <jok10@case.edu>
2020-10-03 10:26:33 -0400

    Added the radar, with a line rotating animation

---
+++ Assets/Scripts/Radar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    private Transform sweeperTransform;
+    private float rotSpeed;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        sweeperTransform = transform.Find("RadarLine");
+        rotSpeed  = 300f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sweeperTransform.eulerAngles += new Vector3(0,0,-rotSpeed * Time.deltaTime);
+    }
+}
