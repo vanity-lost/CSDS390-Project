38c5ea59295a8676ecae4d85f14d175fe4ef3f91
Randolph Zhao <randolph.zhao.49@gmail.com>
2020-10-04 12:28:39 -0400

    commit

--- Assets/Scripts/Connect Wire/QTESys.cs
+++ Assets/Scripts/Connect Wire/QTESys.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
