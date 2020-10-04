20870b2fd605012dd6698ccc4f0b4f49db2bb478
Sam <sammystinky@gmail.com>
2020-10-04 10:08:26 -0400

    Replace Fuse Skeleton

---
+++ Assets/Scripts/Repair Fuse/DistanceTo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceTo : MonoBehaviour
+{
+    [SerializeField] GameObject fuse; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //fuses = GameObject.FindObjectsOfType<LeakCovered>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, fuse.transform.position);
+        //Debug.Log(distance);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Fuse")
+        {
+            Debug.Log(collision);
+            //.collider.GetComponent<MoveFuse>().lockPlace();
+        }
+        //Debug.Log(collision);
+    }
+}
---
+++ Assets/Scripts/Repair Fuse/MoveFuse.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class MoveFuse : MonoBehaviour
+{
+    Vector3 mousePosition;
+    [SerializeField] float distanceLock = 0.2f;
+    [SerializeField] bool lockPlace = false;
+    GameObject[] fuses;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuses = GameObject.FindGameObjectsWithTag("Fuse");
+        Debug.Log(fuses);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnMouseDrag()
+    {
+        if (lockPlace == false) {
+        //held = true;
+        Camera.main.WorldToScreenPoint(transform.position);    
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            mousePosition = hit.point;
+            mousePosition.y = 1.0f;
+        }
+        transform.position = mousePosition;
+        //Gizmos.DrawWireSphere(mousePosition, 2.0f);
+
+        }
+    }
+
+    public void OnMouseUp()
+    {
+        //Debug.Log("Mouse Up");
+        foreach (GameObject fuse in fuses)
+        {
+            float distance = Vector3.Distance(transform.position, fuse.transform.position);
+            //Debug.Log(distance);
+            if (distance < distanceLock)
+            {
+                Debug.Log("In place");
+            }
+        }
+    }
+
+/*
+    public void LockPlace()
+    {
+        lockPlace = true;
+        Debug.Log("Lock");
+    }
+    */
+}
