8f2e7330d6dea7908de3c8cd4a03b7da6404e210
Lina-Dong <59942753+Lina-Dong@users.noreply.github.com>
2020-10-03 22:26:19 -0400

    Press "o" to play Fire Extinguisher minitask

---
+++ Assets/Scripts/Fire Extinguish/ExtinguisherMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExtinguisherMove : MonoBehaviour
+{
+    public RectTransform Extinguisher;
+    public Vector3 offset;
+    public RectTransform backgroundPanel;
+    public Camera cam;
+    public bool takeExtinguisher;
+    public Vector3 originalPos;
+    public GameObject takeBoolText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        takeExtinguisher = false;
+        takeBoolText.GetComponent<Text>().text = "" + takeExtinguisher + "";
+        originalPos = Extinguisher.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            takeExtinguisher = !takeExtinguisher;
+            takeBoolText.GetComponent<Text>().text = "" + takeExtinguisher + "";
+        }
+
+        if (takeExtinguisher == true)
+        {
+            MoveExtinguisher();
+        }
+        else
+        {
+            Extinguisher.position = originalPos;
+        }
+    }
+
+    public void MoveExtinguisher()
+    {
+        Vector3 pos = Input.mousePosition + offset;
+        pos.z = backgroundPanel.position.z;
+        Extinguisher.position = cam.ScreenToWorldPoint(pos);
+    }
+}
---
+++ Assets/Scripts/Fire Extinguish/TaskDone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TaskDone : MonoBehaviour
+{
+    public GameObject Fire1;
+    public GameObject Fire2;
+    public GameObject Fire3;
+    public GameObject takeBoolText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Fire1.activeSelf && !Fire2.activeSelf && !Fire3.activeSelf)
+        {
+            //takeBoolText.GetComponent<Text>().text = "False";
+            StartCoroutine(EndTask());
+        }
+    }
+
+    IEnumerator EndTask()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("all fire are put down");
+        SceneManager.LoadScene("Main");
+    }
+}
