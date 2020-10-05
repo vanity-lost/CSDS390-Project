9a3af7dab8db109fc0515b22ba1b0874797ab361
Randolph Zhao <randolph.zhao.49@gmail.com>
2020-10-05 09:55:13 -0400

    Added fullscreen/windowed choice

---
+++ Assets/Scripts/UI/fullscreenSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fullscreenSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toFullscreen() 
+    {
+        Screen.fullScreen = true;
+    }
+
+    public void toWindowed() 
+    {
+        Screen.fullScreen = false;
+    }
+}
