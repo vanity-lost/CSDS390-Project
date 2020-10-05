bfe42e9db986ea97a778496321967a541cc0bd2a
bentrab <btrabold615@gmail.com>
2020-10-05 17:23:18 -0400

    Added scipt for radar task and started engine silence

---
+++ Assets/Scripts/Turn Radar OnOff/Radar.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RadarStart : MonoBehaviour // TODO timer needs some work
+{
+    public GameObject Panel;
+    //public RadarTimer Timer;
+    public string correctCode = "afge";
+    public string charClicked;
+    public bool newInputDetected;
+    public int numClicked; // can be randomly generated along w/ code
+    public int numLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        numLeft = 4;
+        newInputDetected = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numClicked <= correctCode.Length) 
+        {
+            if (newInputDetected == true) 
+            {
+                if(correctCode.Contains(charClicked)) 
+                {
+                    //TODO make input button disappear
+                    Debug.Log("Correct character clicked");
+                    numLeft--;
+                }
+                else 
+                {
+                    StartCoroutine(ReloadTask());
+                    //StartCoroutine(reload this scene); 
+                }
+                newInputDetected = false;
+            }
+        }
+        if (numLeft == 0)//radar on or off
+        {
+
+            StartCoroutine(EndTask());
+        }
+    }
+
+    public void btnClickDetected(string btnChar)
+    {
+        numClicked++;
+        charClicked = btnChar;
+        newInputDetected = true;
+        Debug.Log("button clicked:" + charClicked);
+    }
+
+    IEnumerator EndTask()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("Correct Characters");
+        SceneManager.LoadScene("Main");
+    }
+
+    IEnumerator ReloadTask()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("Time Ran Out!!!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/Turn Radar OnOff/RadarTimer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public Text timeText;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
