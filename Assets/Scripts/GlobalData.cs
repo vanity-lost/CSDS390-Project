ccb8d62e0f214ff059243139abf1fa8ef4c4ea74
Sam <sammystinky@gmail.com>
2020-10-06 12:37:42 -0400

    Global Data For Minigame State
    Can only play minigame if broken

---
+++ Assets/Scripts/GlobalData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalData : MonoBehaviour
+{ 
+    [SerializeField] static public bool lights = true;
+    [SerializeField] static public bool engineBroken = false;
+    [SerializeField] static public bool wiresBroken = false;
+    [SerializeField] static public bool hullBroken = false;
+    [SerializeField] static public bool fires = false;
+    [SerializeField] static public bool fuseBroken = false;
+    [SerializeField] static public bool storageLocked = false;
+    [SerializeField] static public bool updateEngine = false;
+
+
+    private void Start()
+    {
+        fuseBroken = true;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
--- Assets/Scripts/PlayMinigame.cs
+++ Assets/Scripts/PlayMinigame.cs
@@ -7,40 +7,96 @@ public class PlayMinigame : MonoBehaviour
 {
     [SerializeField] GameObject engine;
     [SerializeField] float distance = 5f;
+
+    [SerializeField] private bool engineBroken = false;
+    [SerializeField] private bool wiresBroken = false;
+    [SerializeField] private bool hullBroken = false;
+    [SerializeField] private bool fires = false;
+    [SerializeField] private bool fuseBroken = false;
+    [SerializeField] private bool storageLocked = false;
+
+    private bool engineUpdate = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        engineBroken = true;
+        fuseBroken = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (!dialogueUpdate.locked) {
-            if (Input.GetKeyDown("q") & (Vector3.Distance(transform.position, engine.transform.position) < distance))
+            if (Input.GetKeyDown("q") & (Vector3.Distance(transform.position, engine.transform.position) < distance) & GlobalData.engineBroken)
             {
                 SceneManager.LoadScene("Fix Engine");
             }
-            if (Input.GetKeyDown("l"))
+            if (Input.GetKeyDown("l") & GlobalData.wiresBroken)
             {
                 SceneManager.LoadScene("Connect Wire");
             }
-            if (Input.GetKeyDown("k"))
+            if (Input.GetKeyDown("k") & GlobalData.storageLocked)
             {
                 SceneManager.LoadScene("Storage Room");
             }
-            if (Input.GetKeyDown("h"))
+            if (Input.GetKeyDown("h") & GlobalData.hullBroken)
             {
                 SceneManager.LoadScene("Fix Hull");
             }
-            if (Input.GetKeyDown("o"))
+            if (Input.GetKeyDown("o") & GlobalData.fires)
             {
                 SceneManager.LoadScene("Fire Extinguish");
+            }
+            if (Input.GetKeyDown("f") & GlobalData.fuseBroken)
+            {
+                SceneManager.LoadScene("Repair Fuse");
             }
             if (Input.GetKeyDown("e"))
             {
                 SceneManager.LoadScene("End Scene");
             }
         }
+        if (Time.time > 30 &  GlobalData.updateEngine == false) 
+        {
+            Debug.Log("Broke Engine");
+            GlobalData.updateEngine = true;
+            GlobalData.engineBroken = true;
+        }
     }
+    
+
+    /*
+    public void FuseBroken(bool status)
+    {
+        fuseBroken = status;
+    }
+
+    public void EngineBroken(bool status)
+    {
+        engineBroken = status;
+    }
+
+    public void Fires(bool status)
+    {
+        fires = status;
+    }
+
+    public void HullBroken(bool status)
+    {
+        fuseBroken = status;
+    }
+
+    public void WiresBroken(bool status)
+    {
+        fuseBroken = status;
+    }
+
+    public void StorageLocked(bool status)
+    {
+        storageLocked = status;
+    }
+    */
+
+
 }
