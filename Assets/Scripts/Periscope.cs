e5a73ae3f955c07eebd288cb50db4e42c97761f6
Alvin-Kong <axk1079@case.edu>
2020-10-06 07:26:10 -0700

    Edits to Periscope UI

---
+++ Assets/Scripts/Periscope.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Periscope : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "periscope")
+                {
+                    Debug.Log("Periscope Hit");
+                    SceneManager.LoadScene("Periscope View");
+                    Debug.Log("Loading Environment Scene");
+                }
+            }
+        }
+
+        //if (Physics.Raycast (ray, outhit))
+        //{
+        //    if (hit.transform.name == "Parascope:pCylinder21") {
+        //        SceneManager.LoadScene("Environment");
+        //    }
+        //}
+    }
+}
