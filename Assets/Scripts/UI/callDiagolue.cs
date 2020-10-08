890664935fd1d50f6d5257f13b2db8cc5781a4e3
Alvin-Kong <axk1079@case.edu>
2020-10-08 03:05:28 -0700

    removed the repetative tutorial

--- Assets/Scripts/UI/callDiagolue.cs
+++ Assets/Scripts/UI/callDiagolue.cs
@@ -6,23 +6,35 @@ public class callDiagolue : MonoBehaviour
 {
     float timer = 0;
     public GameObject TutorialCanvas;
-    private bool trigger = false;
+    public GameObject UserCanvas;
+    private static bool trigger = false;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!trigger) {
-            timer += Time.deltaTime;
-            if (timer >= 1) {
-                TutorialCanvas.SetActive(true);
-                trigger = true;
-            }
+        if (!trigger)
+        {
+            TutorialCanvas.SetActive(true);
+            trigger = true;
+        }
+        else
+        {
+            UserCanvas.SetActive(true);
         }
     }
+
+    //void Update()
+    //{
+    //    if (!trigger) {
+    //        timer += Time.deltaTime;
+    //        if (timer >= 1) {
+    //            TutorialCanvas.SetActive(true);
+    //            trigger = true;
+    //            counter++;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        GameObject.Find("UserCanvas").SetActive(true);
+    //    }
+    //}
 }
