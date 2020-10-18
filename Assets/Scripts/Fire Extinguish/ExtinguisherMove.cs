190bff3d49e5aa7e48817b748d6148f89ac1aba5
Lina-Dong <59942753+Lina-Dong@users.noreply.github.com>
2020-10-17 21:08:39 -0400

    Fire Extinguisher task modified
    apply extinguisher model, adjust the moving of extingusiher

--- Assets/Scripts/Fire Extinguish/ExtinguisherMove.cs
+++ Assets/Scripts/Fire Extinguish/ExtinguisherMove.cs
@@ -6,19 +6,27 @@ using UnityEngine.UI;
 public class ExtinguisherMove : MonoBehaviour
 {
     public RectTransform Extinguisher;
+    public GameObject FireExtinguisher;
     public Vector3 offset;
+    public float zCoord;
     public RectTransform backgroundPanel;
-    public Camera cam;
     public bool takeExtinguisher;
-    public Vector3 originalPos;
+    public Vector3 extinguisherOriginalPos;//3d model's original pos
     public GameObject takeBoolText;
+    public float distanceFromCam;
+    //public float distanceFromCam = 4.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         takeExtinguisher = false;
         takeBoolText.GetComponent<Text>().text = "" + takeExtinguisher + "";
-        originalPos = Extinguisher.position;
+        extinguisherOriginalPos = FireExtinguisher.transform.position;
+
+        Vector3 tovector = transform.position - Camera.main.transform.position;
+        Vector3 lineardistance = Vector3.Project(tovector, Camera.main.transform.forward);
+        distanceFromCam = lineardistance.magnitude;
     }
 
     // Update is called once per frame
@@ -28,26 +36,29 @@ public class ExtinguisherMove : MonoBehaviour
         {
             takeExtinguisher = !takeExtinguisher;
             takeBoolText.GetComponent<Text>().text = "" + takeExtinguisher + "";
+            
         }
 
-        if (takeExtinguisher == true)
+            if (takeExtinguisher == true)
         {
             MoveExtinguisher();
         }
         else
         {
-            Extinguisher.position = originalPos;
+            FireExtinguisher.transform.position = extinguisherOriginalPos;
         }
     }
 
     public void MoveExtinguisher()
     {
-        Vector3 pos = Input.mousePosition + offset;
-        pos.z = backgroundPanel.position.z;
-        //pos.z = backgroundPanel.position.z-420;
-        Vector3 temppos = cam.ScreenToWorldPoint(pos);
-        //temppos.z = 10;
-        Extinguisher.position = temppos;
-        //Extinguisher.position = cam.ScreenToViewportPoint(pos);
+        //Vector3 mousepos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z+transform.position.z);
+        //Vector3 objposition = Camera.main.ScreenToWorldPoint(mousepos);
+        //transform.position = objposition;
+        Vector3 mousepos = Input.mousePosition;
+        mousepos.z = distanceFromCam;
+        transform.position = Camera.main.ScreenToWorldPoint(mousepos)+offset;
+        
+
+
     }
 }
