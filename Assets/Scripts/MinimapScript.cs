b5842b06db66092a27ce161a58640228f38611c9
JKupoluyi <jok10@case.edu>
2020-10-19 23:18:56 -0400

    Added minimap

---
+++ Assets/Scripts/MinimapScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapScript : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+
+}
