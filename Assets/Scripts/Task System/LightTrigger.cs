d06fb197d4fd5b92f4cb64f4d2e00ae510c91cf7
Randolph Zhao <randolph.zhao.49@gmail.com>
2020-10-21 15:07:45 -0400

    update emergency light trigger

--- Assets/Scripts/Task System/LightTrigger.cs
+++ Assets/Scripts/Task System/LightTrigger.cs
@@ -4,36 +4,25 @@ using UnityEngine;
 
 public class LightTrigger : MonoBehaviour
 {
-    public bool lightTrigger = false;
-    float timer = 0;
+    public bool lightTrigger = true;
 
     public GameObject EmergencyLight1;
     public GameObject EmergencyLight2;
     public GameObject EmergencyLight3;
     public GameObject EmergencyLight4;
-    // Start is called before the first frame update
-    void Start()
-    {
-        timer = 0;
-    }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= 30 && !lightTrigger) 
-        {
-            lightTrigger = true;
-            timer = 0;
-        }
-        if (lightTrigger) 
+        
+        if (GlobalData.wiresBroken || GlobalData.fuseBroken) 
         {
             EmergencyLight1.SetActive(true);
             EmergencyLight2.SetActive(true);
             EmergencyLight3.SetActive(true);
             EmergencyLight4.SetActive(true);
         }
-        if (!lightTrigger) 
+        if (!GlobalData.wiresBroken && !GlobalData.fuseBroken) 
         {
             EmergencyLight1.SetActive(false);
             EmergencyLight2.SetActive(false);
