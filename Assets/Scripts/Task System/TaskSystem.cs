4490fb1567463f297defd7ec2d81b4a573498990
Randolph Zhao <randolph.zhao.49@gmail.com>
2020-10-26 14:14:42 -0400

    Update the task system

--- Assets/Scripts/Task System/TaskSystem.cs
+++ Assets/Scripts/Task System/TaskSystem.cs
@@ -17,8 +17,12 @@ public class TaskSystem : MonoBehaviour
     public AudioMixer mixer;
 
     float timer = 0;
+    float timer2 = 0;
 
-    bool trigger = false;
+    bool triggerFire = false;
+    bool triggerEngine = false;
+    bool triggerFuse = false;
+    bool triggerWire = false;
 
     // Start is called before the first frame update
     void Start()
@@ -31,42 +35,44 @@ public class TaskSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        if (!GlobalData.hullBroken && !GlobalData.updateHull) {
-            if (trigger) {
-                trigger = false;
-                timer = 0;
-            } else {
-                timer += Time.deltaTime;
-            }
+        if (!GlobalData.hullBroken) {
+            timer2 = 0;
+            triggerFire = false;
+            triggerEngine = false;
+            triggerFuse = false;
+            triggerWire = false;
+            timer += Time.deltaTime;
         }
-        if (timer >= 10) {
-            if (Random.Range(0,6) == 1) {
+        if (timer >= 20) {
+            if (Random.Range(0,5) == 1) {
+                timer = 0;
                 GlobalData.hullBroken = true;
                 GlobalData.updateHull = true;
             }
         }
-        if (GlobalData.hullBroken && GlobalData.updateHull) {
-            GlobalData.fires = true;
-            GlobalData.updateFire = true;
-            if (Random.Range(0,2) == 1) {
+        if (GlobalData.hullBroken) {
+            timer2 += Time.deltaTime;
+            if (timer2 > 10 && !triggerFire) {
+                triggerFire = true;
+                GlobalData.fires = true;
+                GlobalData.updateFire = true;
+            }
+            if (timer2 > 15 && Random.Range(0,10) == 1 && !triggerEngine) {
+                triggerEngine = true;
                 GlobalData.engineBroken = true;
                 GlobalData.updateEngine = true;
             }
-            if (Random.Range(0,5) == 1) {
+            if (timer2 > 30 && Random.Range(0,10) == 1 && !triggerFuse) {
+                triggerFuse = true;
                 GlobalData.fuseBroken = true;
             }
-            if (Random.Range(0,10) == 1) {
+            if (timer2 > 30 && Random.Range(0,10) == 1 && !triggerWire) {
+                triggerWire = true;
                 GlobalData.wiresBroken = true;
                 GlobalData.updateWires = true;
             }
         }
 
-        updatePanel();
-
-        checkGame();
-    }
-
-    private void updatePanel() {
         taskInfo = title;
         SubDistanceTracker.isMoving = true;
         if (hint) {
@@ -76,11 +82,6 @@ public class TaskSystem : MonoBehaviour
 
         if (GlobalData.storageLocked)
         {
-<<<<<<< HEAD
-            hint = false;
-        }
-        if (GlobalData.engineBroken && GlobalData.updateEngine) {
-=======
             taskInfo += "-    Storage Locked!\n";
             newTaskHint.SetActive(true);
             SubDistanceTracker.isMoving = false;  
@@ -88,7 +89,6 @@ public class TaskSystem : MonoBehaviour
         } 
 
         if (GlobalData.engineBroken) {
->>>>>>> 9483329ac0fc8592465650f03cacbd871b876f29
             taskInfo += "-    Engine Broken!\n";
             newTaskHint.SetActive(true);
             SubDistanceTracker.isMoving = false;      
@@ -97,7 +97,7 @@ public class TaskSystem : MonoBehaviour
         if (GlobalData.fires) {
             taskInfo += "-    Fires in the submarine!\n";
             newTaskHint.SetActive(true);
-            SubHealth.healthNum -= Time.deltaTime * 0.5f;
+            SubHealth.healthNum -= Time.deltaTime * 0.2f;
         }
 
         if (GlobalData.wiresBroken) {
@@ -110,7 +110,7 @@ public class TaskSystem : MonoBehaviour
         if (GlobalData.hullBroken) {
             taskInfo += "-    Hull Broken!\n";
             newTaskHint.SetActive(true);
-            SubHealth.healthNum -= Time.deltaTime * 1f;
+            SubHealth.healthNum -= Time.deltaTime * 0.5f;
         }
 
         if (GlobalData.fuseBroken) {
@@ -124,10 +124,7 @@ public class TaskSystem : MonoBehaviour
         }
         
         _dialogue.SetText(taskInfo);
-    }
 
-    public void checkGame()
-    {
         if (energy.energyNum <= 0 || SubHealth.healthNum <= 0) {
             ESCDectect.gameIsPaused = true;
             SceneManager.LoadScene("Death Scene");
@@ -137,4 +134,5 @@ public class TaskSystem : MonoBehaviour
             SceneManager.LoadScene("End Scene");
         }
     }
+
 }
