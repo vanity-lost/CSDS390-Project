7ea9bf916c47a04314c81dee4a512b76531f20e8
unknown <johnkwak7@gmail.com>
2020-10-26 21:19:50 -0400

    Created new model to store minigame data without passing around full gameobject

--- Assets/Scripts/CreatureBehavior/indoorCreatureBehaviorTemp.cs
+++ Assets/Scripts/CreatureBehavior/indoorCreatureBehaviorTemp.cs
@@ -1,14 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.AI;
+
+public class minigameModel
+{
+    private Vector3 _location;
+    private string _minigameName;
+
+    public Vector3 location
+    {
+        get
+        {
+            return _location;
+        }
+        set
+        {
+            _location = value;
+        }
+    }
+
+    public string _minigameName
+    {
+        get
+        {
+            return _minigameName;
+        }
+        set
+        {
+            _minigameName = value;
+        }
+    }
+}
 
 public class indoorCreatureBehaviorTemp : MonoBehaviour
 {
     //const Transform[] SUB_ROOMS = [];
+    const float minGoalDistanceFromPlayer = 100f;
 
     public GameObject player;
-    public GameObject[] minigameTriggerLocations;
+    public GameObject[] minigameObjects;
     private Transform goal;
     private NavMeshAgent agent;
+    private nextMinigameSabatageLocation;
 
     void Start()
     {
@@ -32,5 +65,17 @@ public class indoorCreatureBehaviorTemp : MonoBehaviour
     private void runFromPlayer()
     {
         agent.destination = -1 * player.transform.position;
+    }
+
+    private Vector3 nextMinitaskSabatageLocation()
+    {
+        Vector3[] locationsAwayFromPlayer;
+
+        foreach(GameObject minigameObject in minigameObjects)
+        {
+            if ((player.transform.position - minigameObject.transform.position).magnitude >= minGoalDistanceFromPlayer)
+        }
+
+        Random.Range(0, minigameObjects.Length - 1)
     }
 }
