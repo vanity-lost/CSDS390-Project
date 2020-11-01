d94b4d27752325897e9e564215db323b0753eecd
Sam <sammystinky@gmail.com>
2020-11-01 10:52:50 -0500

    Auto Formatting MiniTaskStarter, hull appears broken

--- Assets/Scripts/Task System/MiniTaskStarter.cs
+++ Assets/Scripts/Task System/MiniTaskStarter.cs
@@ -45,114 +45,148 @@ public class MiniTaskStarter : MonoBehaviour
         lights = lightHolder.GetComponentsInChildren<Light>();
     }
 
-  void Update()
+    void Update()
     {
-        if (!dialogueUpdate.locked) {
-            if(GlobalData.engineBroken) {
+        if (!dialogueUpdate.locked)
+        {
+            if (GlobalData.engineBroken)
+            {
                 EngineRoomSpotter.SetActive(true);
-                if(Input.GetKeyDown("e") && engine.GetComponent<MinigameTrigger>().getTriggerStatus() && GlobalData.engineBroken) {
-                    if (GlobalData.storageLocked) {
+                if (Input.GetKeyDown("e") && engine.GetComponent<MinigameTrigger>().getTriggerStatus() && GlobalData.engineBroken)
+                {
+                    if (GlobalData.storageLocked)
+                    {
                         storageHint.SetActive(true);
                         TaskSystem.hint = true;
                     }
-                    else{
+                    else
+                    {
                         ESCDectect.gameIsPaused = true;
                         SceneManager.LoadScene("Fix Engine");
                     }
                     ESCDectect.gameIsPaused = true;
                     SceneManager.LoadScene("Fix Engine");
-                } 
-            } else {
+                }
+            }
+            else
+            {
                 EngineRoomSpotter.SetActive(false);
             }
 
-            if(GlobalData.wiresBroken) {
+            if (GlobalData.wiresBroken)
+            {
                 //Set the WireBox;
-                    if (GlobalData.brokenWireboxLoc == 1) {
-                        WireHeadSpotter.SetActive(true);
-                        wireboxhead.GetComponent<WireBoxTrigger>().setBrokenStatus(true);
-                    }
-                    else if (GlobalData.brokenWireboxLoc == 2) {
-                        WireMidSpotter.SetActive(true);
-                        wireboxmid.GetComponent<WireBoxTrigger>().setBrokenStatus(true);
-                    }
-                    else if (GlobalData.brokenWireboxLoc == 3) {
-                        WireLowSpotter.SetActive(true);
-                        wireboxtail.GetComponent<WireBoxTrigger>().setBrokenStatus(true);
-                    }
-
-                    if (wireboxhead.GetComponent<WireBoxTrigger>().getTriggerStatus() && wireboxhead.GetComponent<WireBoxTrigger>().getBrokenStatus()) {
-                        ESCDectect.gameIsPaused = true;
-                        SceneManager.LoadScene("Connect Wire");
-                    } else if(wireboxmid.GetComponent<WireBoxTrigger>().getTriggerStatus() && wireboxmid.GetComponent<WireBoxTrigger>().getBrokenStatus()) {
-                        ESCDectect.gameIsPaused = true;
-                        SceneManager.LoadScene("Connect Wire");
-                    } else if(wireboxtail.GetComponent<WireBoxTrigger>().getTriggerStatus() && wireboxtail.GetComponent<WireBoxTrigger>().getBrokenStatus()) {
-                        ESCDectect.gameIsPaused = true;
-                        SceneManager.LoadScene("Connect Wire");
-                    }
-                } else {
-                    WireHeadSpotter.SetActive(false);
-                    wireboxhead.GetComponent<WireBoxTrigger>().setBrokenStatus(false);
-
-                    WireMidSpotter.SetActive(false);
-                    wireboxmid.GetComponent<WireBoxTrigger>().setBrokenStatus(false);
-
-                    WireLowSpotter.SetActive(false);
-                    wireboxtail.GetComponent<WireBoxTrigger>().setBrokenStatus(false);
+                if (GlobalData.brokenWireboxLoc == 1)
+                {
+                    WireHeadSpotter.SetActive(true);
+                    wireboxhead.GetComponent<WireBoxTrigger>().setBrokenStatus(true);
+                }
+                else if (GlobalData.brokenWireboxLoc == 2)
+                {
+                    WireMidSpotter.SetActive(true);
+                    wireboxmid.GetComponent<WireBoxTrigger>().setBrokenStatus(true);
+                }
+                else if (GlobalData.brokenWireboxLoc == 3)
+                {
+                    WireLowSpotter.SetActive(true);
+                    wireboxtail.GetComponent<WireBoxTrigger>().setBrokenStatus(true);
                 }
 
-                if (GlobalData.storageLocked ) {
-                    StorageRoomSpotter.SetActive(true);
-                    if(Input.GetKeyDown("e") & storage.GetComponent<MinigameTrigger>().getTriggerStatus()) {
-                        ESCDectect.gameIsPaused = true;
-                        SceneManager.LoadScene("Storage Room");
-                    }
-                } else {
-                    StorageRoomSpotter.SetActive(false);
+                if (wireboxhead.GetComponent<WireBoxTrigger>().getTriggerStatus() && wireboxhead.GetComponent<WireBoxTrigger>().getBrokenStatus())
+                {
+                    ESCDectect.gameIsPaused = true;
+                    SceneManager.LoadScene("Connect Wire");
                 }
-
-                if(GlobalData.hullBroken) {
-                    HullSpotter.SetActive(true);
-                    if (Input.GetKeyDown("e") && hull.GetComponent<MinigameTrigger>().getTriggerStatus()) {
-                        ESCDectect.gameIsPaused = true;
-                        SceneManager.LoadScene("Fix Hull");
-                    }
-                } else {
-                    HullSpotter.SetActive(false);
+                else if (wireboxmid.GetComponent<WireBoxTrigger>().getTriggerStatus() && wireboxmid.GetComponent<WireBoxTrigger>().getBrokenStatus())
+                {
+                    ESCDectect.gameIsPaused = true;
+                    SceneManager.LoadScene("Connect Wire");
                 }
-
-                if(GlobalData.fires) {
-                    FireEffect.SetActive(true);
-                    FireSpotter.SetActive(true);
-                    if (Input.GetKeyDown("e") & fireExtinguisher.GetComponent<MinigameTrigger>().getTriggerStatus()) {
-                        ESCDectect.gameIsPaused = true;
-                        SceneManager.LoadScene("Fire Extinguish");
-                    }
-                } else {
-                    FireSpotter.SetActive(false);
-                    FireEffect.SetActive(false);
+                else if (wireboxtail.GetComponent<WireBoxTrigger>().getTriggerStatus() && wireboxtail.GetComponent<WireBoxTrigger>().getBrokenStatus())
+                {
+                    ESCDectect.gameIsPaused = true;
+                    SceneManager.LoadScene("Connect Wire");
                 }
+            }
+            else
+            {
+                WireHeadSpotter.SetActive(false);
+                wireboxhead.GetComponent<WireBoxTrigger>().setBrokenStatus(false);
 
-                if(GlobalData.fuseBroken) {
-                    FuseSpotter.SetActive(true);
-                    if (Input.GetKeyDown("e") & fuse.GetComponent<MinigameTrigger>().getTriggerStatus()) {
-                        ESCDectect.gameIsPaused = true;
-                        SceneManager.LoadScene("Repair Fuse");
-                    }
-                } else {
-                    FuseSpotter.SetActive(false);
+                WireMidSpotter.SetActive(false);
+                wireboxmid.GetComponent<WireBoxTrigger>().setBrokenStatus(false);
+
+                WireLowSpotter.SetActive(false);
+                wireboxtail.GetComponent<WireBoxTrigger>().setBrokenStatus(false);
+            }
+
+            if (GlobalData.storageLocked)
+            {
+                StorageRoomSpotter.SetActive(true);
+                if (Input.GetKeyDown("e") & storage.GetComponent<MinigameTrigger>().getTriggerStatus())
+                {
+                    ESCDectect.gameIsPaused = true;
+                    SceneManager.LoadScene("Storage Room");
                 }
-       
+            }
+            else
+            {
+                StorageRoomSpotter.SetActive(false);
+            }
 
-                if (Input.GetKeyDown("e") & lightSwitch.GetComponent<MinigameTrigger>().getTriggerStatus())
-                    {
-                        Debug.Log(GlobalData.lightSwitch);
-                        GlobalData.lightSwitch = !GlobalData.lightSwitch;
-                        Debug.Log("Lights Flipped");
-                        Debug.Log(GlobalData.lightSwitch);
-                    }
-         }
+            if (GlobalData.hullBroken)
+            {
+                HullSpotter.SetActive(true);
+                if (Input.GetKeyDown("e") && hull.GetComponent<MinigameTrigger>().getTriggerStatus())
+                {
+                    ESCDectect.gameIsPaused = true;
+                    SceneManager.LoadScene("Fix Hull");
+                }
+            }
+            else
+            {
+                HullSpotter.SetActive(false);
+            }
+
+            if (GlobalData.fires)
+            {
+                FireEffect.SetActive(true);
+                FireSpotter.SetActive(true);
+                if (Input.GetKeyDown("e") & fireExtinguisher.GetComponent<MinigameTrigger>().getTriggerStatus())
+                {
+                    ESCDectect.gameIsPaused = true;
+                    SceneManager.LoadScene("Fire Extinguish");
+                }
+            }
+            else
+            {
+                FireSpotter.SetActive(false);
+                FireEffect.SetActive(false);
+            }
+
+            if (GlobalData.fuseBroken)
+            {
+                FuseSpotter.SetActive(true);
+                if (Input.GetKeyDown("e") & fuse.GetComponent<MinigameTrigger>().getTriggerStatus())
+                {
+                    ESCDectect.gameIsPaused = true;
+                    SceneManager.LoadScene("Repair Fuse");
+                }
+            }
+            else
+            {
+                FuseSpotter.SetActive(false);
+            }
+
+
+            if (Input.GetKeyDown("e") & lightSwitch.GetComponent<MinigameTrigger>().getTriggerStatus())
+            {
+                Debug.Log(GlobalData.lightSwitch);
+                GlobalData.lightSwitch = !GlobalData.lightSwitch;
+                Debug.Log("Lights Flipped");
+                Debug.Log(GlobalData.lightSwitch);
+            }
+        }
 
         if (GlobalData.lightsOn != lights[0].enabled)
         {
@@ -160,9 +194,9 @@ public class MiniTaskStarter : MonoBehaviour
         }
         if (GlobalData.lightsOn != (GlobalData.lightSwitch && !GlobalData.wiresBroken && !GlobalData.fuseBroken))
         {
-                Debug.Log("In Here");
-                GlobalData.lightsOn = !GlobalData.lightsOn;
-                LightsFlip();
+            Debug.Log("In Here");
+            GlobalData.lightsOn = !GlobalData.lightsOn;
+            LightsFlip();
         }
         //Debug.Log(GlobalData.lightSwitch);
         if (GlobalData.lightSwitch)
@@ -175,7 +209,15 @@ public class MiniTaskStarter : MonoBehaviour
             lightSwitch.transform.localPosition = new Vector3(0.3853737f, 1.660247f, 2.669332f);
             lightSwitch.transform.localRotation = new Quaternion(-0.166412354f, 0.106388971f, -0.82593739f, 0.528030097f);
         }
-    }  
+        if (!GlobalData.hullBroken)
+        {
+            hull.transform.GetChild(0).GetComponent<Renderer>().enabled = false;
+        }
+        else
+        {
+            hull.transform.GetChild(0).GetComponent<Renderer>().enabled = true;
+        }
+    }
 
     public void LightsFlip()
     {
