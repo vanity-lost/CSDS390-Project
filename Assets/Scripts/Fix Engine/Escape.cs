53809b5f7587fad7de76aa9c5db044152a2f6ff0
Sam <sammystinky@gmail.com>
2020-11-06 22:26:36 -0500

    Uncommented Task Escaping For Minitasks

--- Assets/Scripts/Fix Engine/Escape.cs
+++ Assets/Scripts/Fix Engine/Escape.cs
@@ -14,10 +14,11 @@ public class Escape : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.Escape))
-        // {
-        //     ESCDectect.gameIsPaused = false;
-        //     SceneManager.LoadScene("Main");
-        // }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ESCDectect.gameIsPaused = false;
+            SceneManager.LoadScene("Main");
+        }
     }
 }
+
