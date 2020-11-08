b082c1d9287ef3885c840bf0888271f51f1ca9f5
Lina-Dong <59942753+Lina-Dong@users.noreply.github.com>
2020-11-08 07:17:03 -0500

    update the informationCanvas for story and instructions

---
+++ Assets/Scripts/UI/Start Scene/AboutCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboutCanvas : MonoBehaviour
+{
+    public GameObject infoCanvas;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            infoCanvas.SetActive(false);
+        }
+    }
+}
