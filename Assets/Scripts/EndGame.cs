ad802aa1100f847f7eb1fb6e9b750f6a74bb649b
Alvin-Kong <axk1079@case.edu>
2020-11-18 23:05:05 -0800

    end game shortcut: press 'b'

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("b"))
+        {
+            SceneManager.LoadScene("End Scene");
+        }
+    }
+}
